96b43eeac80ce3061571f6306a8d3b9b7a36559b
Ziqiang Li <ziqiang.lee@gmail.com>
2018-03-08 17:50:10 +0800

    modify static path

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const App = new Koa();
 const router = require('./router');
 
 // 静态资源
-App.use(Serve('build'));
+App.use(Serve('./build'));
 // 路由
 App.use(router.routes()).use(router.allowedMethods());
 // 启动程序并监听80
